149c4f0b15461f1cccd4d9f8c9217f76ae90c4d3
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-13 13:08:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import t
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(train, test) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    mean_train = np.mean(train.values)
+    std_train = np.std(train.values, ddof=1)
+    mean_test = np.mean(test.values)
+    std_test = np.std(test.values, ddof=1)
+    # Вычислим доверительный интервал для разности выборочных средних
+    se = np.sqrt((std_train**2)/len(train) + (std_test**2)/len(test))
+    t_stat = t.ppf(0.97, df=(len(train)+len(test)-2))
+    ci_low = (mean_train - mean_test) - t_stat*se
+    ci_high = (mean_train - mean_test) + t_stat*se
+
+    # Проверим, попадает ли ноль в доверительный интервал
+    if (ci_low <= 0).any() and (ci_high >= 0).any():
+        return False
+    else:
+        return True
